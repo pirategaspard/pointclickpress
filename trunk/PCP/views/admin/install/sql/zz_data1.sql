42e02faeeb11ff178a7ad3b51a701be4ccaa8dba
pirategaspard <pirategaspard@b5eaa240-b974-4564-bca8-cd2d19826b34>
2010-06-28 02:20:40 +0000

    deleted event_types sql from install

--- trunk/PCP/views/admin/install/sql/zz_data1.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `event_types` (`id`, `label`, `description`, `event`) VALUES (1, 'Link', 'Create a link to another scene container', 'link');
-
--- trunk/PCP/views/admin/install/sql/zz_data2.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `event_types` (`id`, `label`, `description`, `event`) VALUES (2, 'Trigger', 'Session variable state change', 'trigger');
-
