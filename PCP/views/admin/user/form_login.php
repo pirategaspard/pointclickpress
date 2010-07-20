94c3596c98bb1c79944222bf9f55a6abe031edc6
pirategaspard <dan@danielgaspar.com>
2010-07-20 04:13:55 +0000

    Finally have basic login for admin area

--- PCP/views/admin/user/form_login.php
+++ PCP/views/admin/user/form_login.php
@@ -1,5 +1,9 @@
+<?php 
+	//var_dump($_SESSION);
+?>
+
 <h3>Please Login</h3>
-<form method="post" action="" >
+<form method="post" action="<?php echo($login_form_action); ?>" >
 	Username: <input type="text" id="username" name="username" value="" >
 	Password: <input type="password" id="password" name="password" value="" >
 	<input type="submit" name="submit" value="Log in" />
