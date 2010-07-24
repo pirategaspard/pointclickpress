6a750e320fea61b111c09d758ddc499dae0055cc
pirategaspard <dan@danielgaspar.com>
2010-07-24 18:27:03 +0000

    added very basic ability to choose what your first user is called during install.

--- PCP/views/admin/install/install.php
+++ PCP/views/admin/install/install.php
@@ -4,16 +4,17 @@
 */
 	if (isset($_POST['install']))
 	{
-		include("sql/install.php");		
+		//include("sql/install.php");		
 		
 		// create first user		
-		$user_data['username'] = 'admin';
-		$user_data['password'] = 'admin';
-		$user_data['password2'] = 'admin';
+		$user_data['username'] = $_POST['username'];
+		$user_data['password'] = $_POST['password'];
+		$user_data['password2'] = $_POST['password'];
 		$user_data['email'] = 'admin@localhost';
 		$user_data['active'] = '1';
 		$results = UsersAdmin::create($user_data);
 		
+		//var_dump($results); die();
 		
 		// Done!
 		echo '<h3>Done!</h3>';
@@ -27,6 +28,9 @@
 <h1>Welcome To The PointClickPress Installer!</h1>
 
 <form method="post">
+	<h3>Create Admin User</h3>
+	Username: <input type="text" name="username" value="" />
+	Passsword: <input type="password" name="password" value="" />
 	<input type="submit" name="install" value="install" />
 </form>
 
