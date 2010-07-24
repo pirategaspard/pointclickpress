6f9c919f2d3fe3b02e8fe47015e25f7614b486b1
pirategaspard <pirategaspard@b5eaa240-b974-4564-bca8-cd2d19826b34>
2010-07-24 18:14:28 +0000

    fixed bug in grid.js: now swap background, title, and, description all at once

--- trunk/PCP/js/grid.js
+++ trunk/PCP/js/grid.js
@@ -33,13 +33,13 @@ $(document).ready(function() {
 			{
 				//get scene and swap the data so we don't have to reload the page!
 				var filename = $.getJSON('Scene',function(data){						
-						$('#scene>h1').html(data.title);
-						$('#scene>p').html(data.description);						
 						// pre-load image and then swap background
 						var img = new Image();
 						$(img).load(function() {
 								$('#scene_image').css({backgroundImage:'url('+this.src+')'});
 							}).attr('src', data.filename);
+							$('#scene>h1').html(data.title);
+							$('#scene>p').html(data.description);	
 						});
 			}
 		});
