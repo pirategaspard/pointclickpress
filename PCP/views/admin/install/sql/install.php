2c04435c5c61d4339e1e2d4e14302e7c0baa82aa
pirategaspard <dan@danielgaspar.com>
2010-09-16 02:17:00 +0000

    Updated install file sql

--- PCP/views/admin/install/sql/install.php
+++ PCP/views/admin/install/sql/install.php
@@ -2,11 +2,11 @@
 $q = 'SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO"';
 $results = DB::Query(NULL,$q,FALSE)->execute();
 
-$q = 'CREATE TABLE cells (
-  id bigint(20) unsigned NOT NULL,
-  scene_id bigint(20) unsigned NOT NULL,
-  grid_event_id bigint(20) unsigned NOT NULL,
-  KEY id_storyid (id,scene_id)
+$q = 'CREATE TABLE IF NOT EXISTS `cells` (
+  `id` bigint(20) unsigned NOT NULL,
+  `scene_id` bigint(20) unsigned NOT NULL,
+  `grid_event_id` bigint(20) unsigned NOT NULL,
+  KEY `id_storyid` (`id`,`scene_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8';
 $results = DB::Query(NULL,$q,FALSE)->execute();
 
@@ -55,20 +55,6 @@ INSERT INTO `cells` (`id`, `scene_id`, `grid_event_id`) VALUES
 (97, 24, 21),
 (98, 24, 21),
 (99, 24, 21),
-(90, 25, 22),
-(91, 25, 22),
-(92, 25, 22),
-(93, 25, 22),
-(94, 25, 22),
-(95, 25, 22),
-(96, 25, 22),
-(97, 25, 22),
-(98, 25, 22),
-(99, 25, 22),
-(34, 25, 23),
-(44, 25, 23),
-(45, 25, 23),
-(35, 25, 23),
 (90, 26, 24),
 (91, 26, 24),
 (92, 26, 24),
@@ -217,18 +203,66 @@ INSERT INTO `cells` (`id`, `scene_id`, `grid_event_id`) VALUES
 (97, 22, 37),
 (98, 22, 37),
 (99, 22, 37),
-(72, 40, 0),
-(71, 40, 0),
-(0, 40, 0)';
+(45, 25, 0),
+(44, 25, 0),
+(35, 25, 0),
+(34, 25, 0),
+(0, 25, 0),
+(46, 25, 0),
+(36, 25, 0),
+(33, 25, 0),
+(43, 25, 0),
+(46, 25, 42),
+(45, 25, 42),
+(44, 25, 42),
+(43, 25, 42),
+(36, 25, 42),
+(35, 25, 42),
+(34, 25, 42),
+(33, 25, 42),
+(99, 25, 43),
+(98, 25, 43),
+(97, 25, 43),
+(96, 25, 43),
+(95, 25, 43),
+(94, 25, 43),
+(93, 25, 43),
+(92, 25, 43),
+(91, 25, 43),
+(90, 25, 43),
+(0, 25, 43),
+(0, 25, 43),
+(90, 25, 44),
+(91, 25, 44),
+(92, 25, 44),
+(93, 25, 44),
+(94, 25, 44),
+(95, 25, 44),
+(96, 25, 44),
+(97, 25, 44),
+(98, 25, 44),
+(99, 25, 44),
+(2, 22, 46),
+(12, 22, 46),
+(22, 22, 46),
+(32, 22, 46),
+(71, 40, 47),
+(72, 40, 47),
+(37, 24, 48),
+(36, 24, 48),
+(27, 24, 48),
+(26, 24, 48),
+(16, 22, 45),
+(6, 22, 45)';
 $results = DB::Query(NULL,$q,FALSE)->execute();
 
 
-$q = 'CREATE TABLE containers (
-  story_id bigint(20) unsigned NOT NULL,
-  id bigint(20) unsigned NOT NULL auto_increment,
-  title varchar(255) NOT NULL,
-  PRIMARY KEY  (id)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=18';
+$q = 'CREATE TABLE IF NOT EXISTS `containers` (
+  `story_id` bigint(20) unsigned NOT NULL,
+  `id` bigint(20) unsigned NOT NULL auto_increment,
+  `title` varchar(255) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=30';
 $results = DB::Query(NULL,$q,FALSE)->execute();
 
 /*data*/
@@ -241,13 +275,13 @@ $q = "INSERT INTO `containers` (`story_id`, `id`, `title`) VALUES
 (2, 26, 'tunnel'),
 (2, 27, 'tunnel2'),
 (2, 28, 'Ocean'),
-(2, 29, 'Forest');";
+(2, 29, 'Forest')";
 $results = DB::Query(NULL,$q,FALSE)->execute();
 
-$q = 'CREATE TABLE containers_events (
-  container_id bigint(20) unsigned NOT NULL,
-  event_id bigint(20) unsigned NOT NULL,
-  KEY containerid (container_id)
+$q = 'CREATE TABLE IF NOT EXISTS `containers_events` (
+  `container_id` bigint(20) unsigned NOT NULL,
+  `event_id` bigint(20) unsigned NOT NULL,
+  KEY `containerid` (`container_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8';
 $results = DB::Query(NULL,$q,FALSE)->execute();
 
@@ -256,13 +290,13 @@ $q = "INSERT INTO `containers_events` (`container_id`, `event_id`) VALUES
 (25, 39);";
 $results = DB::Query(NULL,$q,FALSE)->execute(); 
 
-$q = 'CREATE TABLE `events` (
-  id bigint(20) unsigned NOT NULL auto_increment,
-  event varchar(255) character set latin1 NOT NULL,
-  event_label varchar(255) character set latin1 NOT NULL,
-  event_value varchar(255) character set latin1 default NULL,
-  PRIMARY KEY  (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=11';
+$q = 'CREATE TABLE IF NOT EXISTS `events` (
+  `id` bigint(20) unsigned NOT NULL auto_increment,
+  `event` varchar(255) character set latin1 NOT NULL,
+  `event_label` varchar(255) character set latin1 NOT NULL,
+  `event_value` varchar(255) character set latin1 default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=50';
 $results = DB::Query(NULL,$q,FALSE)->execute();
 
 /*data*/
@@ -273,7 +307,6 @@ $q = "INSERT INTO `events` (`id`, `event`, `event_label`, `event_value`) VALUES
 (20, 'event_link', 'link', '22'),
 (21, 'event_link', 'link', '20'),
 (22, 'event_link', 'link', '21'),
-(23, 'event_link', 'link', '25'),
 (24, 'event_link', 'link', '22'),
 (25, 'event_link', 'link', '22'),
 (26, 'event_assign', 'Assign value', '\$grate = ''open'';'),
@@ -288,15 +321,21 @@ $q = "INSERT INTO `events` (`id`, `event`, `event_label`, `event_value`) VALUES
 (35, 'event_assign', 'Assign value', '\$forest = ''no_hammer'';\n\$grate = ''hammer'';'),
 (36, 'event_assign', 'Assign value', '\$forest = ''hammer'';\n\$grate = ''no_hammer'';'),
 (37, 'event_link', 'link', '22'),
-(38, 'event_link', 'link', '29');";
+(38, 'event_link', 'link', '29'),
+(43, 'event_link', 'link', '25'),
+(45, 'event_link', 'link', '21'),
+(46, 'event_message', 'User Message', 'You found a tree!!!'),
+(47, 'event_message', 'User Alert Message', 'You clicked on the tree!'),
+(48, 'event_message', 'User Alert Message', 'You Picked Up the Hammer!'),
+(49, 'event_message', 'User Message', 'Sorry this path doesn''t go anywhere!')";
 $results = DB::Query(NULL,$q,FALSE)->execute();
 
-$q = 'CREATE TABLE grids_events (
-  grid_event_id bigint(20) unsigned NOT NULL auto_increment,
-  scene_id bigint(20) unsigned NOT NULL,
-  event_id bigint(20) unsigned NOT NULL,
-  PRIMARY KEY  (grid_event_id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=5';
+$q = 'CREATE TABLE IF NOT EXISTS `grids_events` (
+  `grid_event_id` bigint(20) unsigned NOT NULL auto_increment,
+  `scene_id` bigint(20) unsigned NOT NULL,
+  `event_id` bigint(20) unsigned NOT NULL,
+  PRIMARY KEY  (`grid_event_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=49';
 $results = DB::Query(NULL,$q,FALSE)->execute();
 
 /*data*/
@@ -314,7 +353,6 @@ $q = "INSERT INTO `grids_events` (`grid_event_id`, `scene_id`, `event_id`) VALUE
 (11, 23, 11),
 (12, 23, 12),
 (13, 25, 13),
-(14, 25, 14),
 (15, 26, 15),
 (16, 23, 16),
 (17, 22, 17),
@@ -322,8 +360,6 @@ $q = "INSERT INTO `grids_events` (`grid_event_id`, `scene_id`, `event_id`) VALUE
 (19, 23, 19),
 (20, 24, 20),
 (21, 24, 21),
-(22, 25, 22),
-(23, 25, 23),
 (24, 26, 24),
 (25, 28, 25),
 (26, 28, 26),
@@ -337,42 +373,74 @@ $q = "INSERT INTO `grids_events` (`grid_event_id`, `scene_id`, `event_id`) VALUE
 (34, 40, 34),
 (35, 40, 35),
 (36, 41, 37),
-(37, 22, 38)";
+(37, 22, 38),
+(42, 25, 43),
+(43, 25, 44),
+(44, 25, 45),
+(45, 22, 46),
+(46, 22, 47),
+(47, 40, 48),
+(48, 24, 49)";
 $results = DB::Query(NULL,$q,FALSE)->execute();
 
-$q = 'CREATE TABLE scenes (
-  id bigint(20) unsigned NOT NULL auto_increment,
-  story_id bigint(20) unsigned NOT NULL,
-  container_id bigint(20) unsigned NOT NULL,
-  title varchar(255) default NULL,
-  description text,
-  filename varchar(255) default NULL,
+
+$q = 'CREATE TABLE IF NOT EXISTS `images` (
+  `id` bigint(20) NOT NULL auto_increment,
+  `story_id` bigint(20) NOT NULL,
+  `filename` varchar(255) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=14';
+$results = DB::Query(NULL,$q,FALSE)->execute();
+
+$q = "INSERT INTO `images` (`id`, `story_id`, `filename`) VALUES
+(1, 2, 'forest.JPG'),
+(2, 2, 'forest2.JPG'),
+(3, 2, 'Image000.jpg'),
+(5, 2, 'Image002.jpg'),
+(6, 2, 'Image003.jpg'),
+(7, 2, 'Image004.jpg'),
+(8, 2, 'pipe1.jpg'),
+(9, 2, 'pipe2.jpg'),
+(10, 2, 'tunnel.jpg'),
+(11, 2, 'tunnel2.jpg'),
+(12, 2, 'ocean.JPG'),
+(13, 2, 'Image001.jpg')";
+$results = DB::Query(NULL,$q,FALSE)->execute();
+
+$q = 'CREATE TABLE IF NOT EXISTS `scenes` (
+  `id` bigint(20) unsigned NOT NULL auto_increment,
+  `story_id` bigint(20) unsigned NOT NULL,
+  `container_id` bigint(20) unsigned NOT NULL,
+  `title` varchar(255) default NULL,
+  `description` text,
+  `filename` varchar(255) default NULL,
+  `image_id` bigint(20) default NULL,
   `value` varchar(255) default NULL,
-  PRIMARY KEY  (id),
-  UNIQUE KEY scene_value (`value`,container_id)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=22';
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `scene_value` (`value`,`container_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=42';
 $results = DB::Query(NULL,$q,FALSE)->execute();
 
 /*data*/
-$q = "INSERT INTO `scenes` (`id`, `story_id`, `container_id`, `title`, `description`, `filename`, `value`) VALUES
-(22, 2, 18, 'Start', 'You are at the start of the trail', 'Image000.jpg', ''),
-(23, 2, 20, 'Scene 2', 'I wonder where the path goes?', 'Image001.jpg', ''),
-(24, 2, 21, 'Scene 3', 'There is a turn ahead', 'Image002.jpg', ''),
-(25, 2, 22, 'Scene 4', '', 'Image004.jpg', ''),
-(28, 2, 25, 'Grate', 'The Path ends abruptly at a grate. Maybe you can break it open!', 'pipe1.jpg', 'hammer'),
-(29, 2, 25, 'Grate', 'A piece of the grate is broken, do you dare climb through?', 'pipe2.jpg', 'open'),
-(36, 2, 26, 'tunnel', '', 'tunnel.jpg', ''),
-(37, 2, 27, 'tunnel2', '', 'tunnel2.jpg', ''),
-(38, 2, 28, 'Ocean - The End!', 'You are at the ocean! \nTHE END', 'ocean.JPG', ''),
-(39, 2, 29, 'Forest', 'You are in the forest. There is a path in front of you.', 'forest.JPG', 'no_hammer'),
-(40, 2, 29, 'Forest', 'You are in the Forest. There is a path in front of you. Someone has left a poorly-drawn sledge hammer here!', 'forest2.JPG', 'hammer'),
-(41, 2, 25, 'Grate', 'The Path ends abruptly at a grate. If only you had something to smash it with...', 'pipe1.jpg', 'no_hammer')";
+$q = "INSERT INTO `scenes` (`id`, `story_id`, `container_id`, `title`, `description`, `filename`, `image_id`, `value`) VALUES
+(22, 2, 18, 'Start', 'You are at the start of the trail', 'Image000.jpg', 3, ''),
+(23, 2, 20, 'Scene 2', 'I wonder where the path goes?', 'Image001.jpg', 13, ''),
+(24, 2, 21, 'Scene 3', 'There is a turn ahead', 'Image002.jpg', 5, ''),
+(25, 2, 22, 'Scene 4', 'the path continues', 'Image004.jpg', 7, ''),
+(28, 2, 25, 'Grate', 'The Path ends abruptly at a grate. Maybe you can break it open!', 'pipe1.jpg', 8, 'hammer'),
+(29, 2, 25, 'Grate', 'A piece of the grate is broken, do you dare climb through?', 'pipe2.jpg', 9, 'open'),
+(36, 2, 26, 'tunnel', '', 'tunnel.jpg', 10, ''),
+(37, 2, 27, 'tunnel2', '', 'tunnel2.jpg', 11, ''),
+(38, 2, 28, 'Ocean - The End!', 'You are at the ocean! \nTHE END', 'ocean.JPG', 12, ''),
+(39, 2, 29, 'Forest', 'You are in the forest. There is a path in front of you.', 'forest.JPG', 1, 'no_hammer'),
+(40, 2, 29, 'Forest', 'You are in the Forest. There is a path in front of you. Someone has left a poorly-drawn sledge hammer here!', 'forest2.JPG', 2, 'hammer'),
+(41, 2, 25, 'Grate', 'The Path ends abruptly at a grate. If only you had something to smash it with...', 'pipe1.jpg', 8, 'no_hammer')";
 $results = DB::Query(NULL,$q,FALSE)->execute();
 
-$q = 'CREATE TABLE scenes_events (
-  scene_id bigint(20) unsigned NOT NULL,
-  event_id bigint(20) unsigned NOT NULL,
-  KEY sceneid (scene_id)
+$q = 'CREATE TABLE IF NOT EXISTS `scenes_events` (
+  `scene_id` bigint(20) unsigned NOT NULL,
+  `event_id` bigint(20) unsigned NOT NULL,
+  KEY `sceneid` (`scene_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8';
 $results = DB::Query(NULL,$q,FALSE)->execute();
 
@@ -382,17 +450,17 @@ $q = "";
 $results = DB::Query(NULL,$q,FALSE)->execute();
 */
 
-$q = 'CREATE TABLE stories (
-  id bigint(20) unsigned NOT NULL auto_increment,
-  title varchar(255) NOT NULL,
-  author varchar(255) NOT NULL,
-  description text NOT NULL,
-  first_scene_container_id bigint(20) unsigned default NULL,
-  grid_x smallint(5) unsigned NOT NULL,
-  grid_y smallint(5) unsigned NOT NULL,
-  create_date timestamp NOT NULL default CURRENT_TIMESTAMP,
-  PRIMARY KEY  (id)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2';
+$q = 'CREATE TABLE IF NOT EXISTS `stories` (
+  `id` bigint(20) unsigned NOT NULL auto_increment,
+  `title` varchar(255) NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `first_scene_container_id` bigint(20) unsigned default NULL,
+  `grid_x` smallint(5) unsigned NOT NULL,
+  `grid_y` smallint(5) unsigned NOT NULL,
+  `create_date` timestamp NOT NULL default CURRENT_TIMESTAMP,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3';
 $results = DB::Query(NULL,$q,FALSE)->execute();
 
 /*data*/
@@ -400,32 +468,32 @@ $q = "INSERT INTO `stories` (`id`, `title`, `author`, `description`, `first_scen
 (2, 'A Walk Down The Cobble Stone Path', 'Dan', 'A Walk through the forest on the mysterious cobblestone path', 18, 10, 10, '2010-07-02 08:36:56')";
 $results = DB::Query(NULL,$q,FALSE)->execute();
 
-$q = 'CREATE TABLE stories_events (
-  story_id bigint(20) unsigned NOT NULL,
-  event_id bigint(20) unsigned NOT NULL,
-  KEY storyid (story_id)
+$q = 'CREATE TABLE IF NOT EXISTS `stories_events` (
+  `story_id` bigint(20) unsigned NOT NULL,
+  `event_id` bigint(20) unsigned NOT NULL,
+  KEY `storyid` (`story_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8';
 $results = DB::Query(NULL,$q,FALSE)->execute();
 
 /*data*/
 $q = "
 INSERT INTO `stories_events` (`story_id`, `event_id`) VALUES
-(2, 36);";
+(2, 36)";
 $results = DB::Query(NULL,$q,FALSE)->execute();
 
 $q = "
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(11) unsigned NOT NULL auto_increment,
   `email` varchar(127) NOT NULL,
-  `username` varchar(32) NOT NULL DEFAULT '',
+  `username` varchar(32) NOT NULL default '',
   `password` char(50) NOT NULL,
-  `active` tinyint(4) DEFAULT '1',
-  `logins` int(10) unsigned NOT NULL DEFAULT '0',
-  `last_ip_address` varchar(15) DEFAULT NULL,
-  `last_login` datetime DEFAULT NULL,
-  `created` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=35 ;";
+  `active` tinyint(4) default '1',
+  `logins` int(10) unsigned NOT NULL default '0',
+  `last_ip_address` varchar(15) default NULL,
+  `last_login` datetime default NULL,
+  `created` datetime default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=36";
 $results = DB::Query(NULL,$q,FALSE)->execute();
 
 ?>
