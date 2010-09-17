008ff8c5b29d2b08b6e6e191466ddf8dd95cd1e8
pirategaspard <pirategaspard@b5eaa240-b974-4564-bca8-cd2d19826b34>
2010-09-17 03:11:05 +0000

    a little cleanup in the config file

--- trunk/PCP/config/config.php
+++ trunk/PCP/config/config.php
@@ -1,11 +1,26 @@
 <?php 
 
+/* 
+	SUPPORTED_SCREENS is a comma seperated list of 
+	screen dimensions such as '640x480,800x600,'
+	must be lowest to highest 
+*/
+$supported_screens = '480x320,640x480,800x600'; //,1024x768
+
+/* 
+	JPG image quality
+*/
+$image_quality = 50;
+
+/* 
+	Size of generated thumbnails
+*/
+$thumbnail_size = '100x100';
 
 /* 
 	What is the name of the temporary storage space for uploaded files? 
 */
 $upload_dir = 'uploads';
-
 
 /* 
 	What is the name of the storage space for media that supports  
@@ -14,19 +29,11 @@ $upload_dir = 'uploads';
 $media_dir = 'media';
 
 /* 
-	SUPPORTED_SCREENS is a comma seperated list of 
-	screen dimensions such as '640x480,800x600,'
-	must be lowest to highest 
-*/
-$supported_screens = '480x320,640x480,800x600'; //,1024x768
-
-
-/* 
 	Define constants
  */
 define('UPLOAD_PATH', $upload_dir);
 define('MEDIA_PATH', $media_dir);
-define('IMAGE_QUALITY', 50);
+define('IMAGE_QUALITY', $image_quality);
 define('SUPPORTED_SCREENS', $supported_screens);
-define('THUMBNAIL_IMAGE_SIZE', '100x100');
+define('THUMBNAIL_IMAGE_SIZE', $thumbnail_size);
 ?>
