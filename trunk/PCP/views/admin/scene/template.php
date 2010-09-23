8654843851d935274c8a79e3b84f28f042cc2855
pirategaspard <pirategaspard@b5eaa240-b974-4564-bca8-cd2d19826b34>
2010-09-23 02:46:53 +0000

    bug fixes from converting admin to session vars

--- trunk/PCP/views/admin/scene/template.php
+++ trunk/PCP/views/admin/scene/template.php
@@ -1,22 +1,23 @@
 <?php 
 
-	//echo($page->id);
-
 	echo($story_info);
 	echo($container_info);
 	echo($scene_info);
 	echo($scene_add);
 	echo($scene_form);
-	echo($event_add); 
-	echo($event_list);
-	echo($grid);
+	if ($scene->id > 0)
+	{
+		echo($event_add); 
+		echo($event_list);
+		echo($grid);
 	
-	if (strlen($scene->filename) > 0)
-	{ 
-		//echo($grid_event_add); 
-		echo($grid_event_form);
-		echo($grid_event_list);  	
-	}
-
-	var_dump($_SESSION);	
+		if (strlen($scene->filename) > 0)
+		{ 
+			//echo($grid_event_add); 
+			echo($grid_event_form);
+			echo($grid_event_list);  	
+		}
+	}	
+	
+	var_dump ($_SESSION);
 ?>
