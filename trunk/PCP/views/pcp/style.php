3f3a2ae82408d497d127d9c6d9a1978e172a6a00
pirategaspard <pirategaspard@b5eaa240-b974-4564-bca8-cd2d19826b34>
2010-09-26 23:47:09 +0000

    grid css update

--- trunk/PCP/views/pcp/style.php
+++ trunk/PCP/views/pcp/style.php
@@ -1,4 +1,4 @@
 <style>
-	#grid {background-image:url(<?php echo($scene->getPath($story->scene_width,$story->scene_height))?>); width: <?php echo($story->scene_width)?>px;height:<?php echo($story->scene_height)?>px; overflow: hidden;}
-	#grid a {display:block; width:<?php print($story->cell_width) ?>px; height: <?php print($story->cell_height) ?>px; float: left;}
+	#grid {background-image:url(<?php echo($scene->getPath($story->scene_width,$story->scene_height))?>); width: <?php echo($story->scene_width)?>px;height:<?php echo($story->scene_height)?>px; }
+	#grid a {width:<?php print($story->cell_width) ?>px; height: <?php print($story->cell_height) ?>px; }
 </style>
