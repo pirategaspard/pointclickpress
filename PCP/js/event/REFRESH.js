21684f13d0d099d57d5b08afd025bd71f6791e91
pirategaspard <dan@danielgaspar.com>
2010-09-29 01:02:22 +0000

    refresh event will now attempt to load low res-image immediately and then replace it with a high res image later

--- PCP/js/event/REFRESH.js
+++ PCP/js/event/REFRESH.js
@@ -3,11 +3,19 @@
 (function( $ ){
   $.fn.REFRESH = function(data) 
   {  
+  	// load temp background using low res image
+  	var css = {
+      	'background-image': 'url('+data.preload_filename+')',
+    	'-moz-background-size':'100% 100%, auto auto'
+    }
+  	$('#grid').css(css);
+  
 	// pre-load image and then swap background
 	var img = new Image();
-	$(img).load(function() {
-			$('#grid').css({backgroundImage:'url('+this.src+')'});
-		}).attr('src', data.filename);
+	$(img).load(function() 
+				{
+					$('#grid').css({backgroundImage:'url('+this.src+')'});
+				}).attr('src', data.filename);
 		document.title = data.title;
 		/* $('#title').html(data.title); */
 		$('#description').html(data.description);	
