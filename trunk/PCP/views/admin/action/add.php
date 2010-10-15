c1baa052052dd79c887401abcf974e01750f85d7
pirategaspard <pirategaspard@b5eaa240-b974-4564-bca8-cd2d19826b34>
2010-10-15 00:56:32 +0000

    removed legacy admin/action directory. Added admin/plugin directory

--- trunk/PCP/views/admin/action/add.php
+++
@@ -1,2 +0,0 @@
-<!-- <a href="<?php echo(Url::site(Route::get('admin')->uri(array('controller'=>'action','action'=>'edit'))).'?story_id='.$story->id.'&location_id='.$location->id.'&scene_id='.$scene->id.'&action_id=0'); ?>">Add Action</a> -->
-
