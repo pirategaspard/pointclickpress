86021ac8e2e534906a460b220e09f244ce3124e2
pirategaspard <pirategaspard@b5eaa240-b974-4564-bca8-cd2d19826b34>
2010-10-17 23:00:21 +0000

    scene now displays scene value for easier navigation

--- trunk/PCP/views/admin/scene/info.php
+++ trunk/PCP/views/admin/scene/info.php
@@ -1,1 +1,1 @@
- / <a href="<?php echo(Url::site(Route::get('admin')->uri(array('controller'=>'scene','action'=>'edit'))).'?story_id='.$scene->story_id.'&location_id='.$scene->location_id.'&scene_id='.$scene->id); ?>"><?php echo($scene->title); ?></a>
+ / <a href="<?php echo(Url::site(Route::get('admin')->uri(array('controller'=>'scene','action'=>'edit'))).'?story_id='.$scene->story_id.'&location_id='.$scene->location_id.'&scene_id='.$scene->id); ?>"><?php echo($scene->title.' '.$scene->value); ?></a>
