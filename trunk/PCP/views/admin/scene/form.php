705060b8dffc4cda359bf8c115f250af389d96e1
pirategaspard <pirategaspard@b5eaa240-b974-4564-bca8-cd2d19826b34>
2010-11-03 04:10:25 +0000

    improved admin layout

--- trunk/PCP/views/admin/scene/form.php
+++ trunk/PCP/views/admin/scene/form.php
@@ -7,27 +7,33 @@
 	<form action="<?php print($scene_form_action); ?>" method="post" enctype="multipart/form-data" >
 		<input type="hidden" name="story_id" value="<?php print($scene->story_id); ?>" >
 		<input type="hidden" name="location_id" value="<?php print($scene->location_id); ?>" >
-		<input type="hidden" name="id" value="<?php print($scene->id); ?>" >		
-		<label>
-			Title
-			<input type="text" name="title" size="50" value="<?php print($scene->title); ?>" />
-		</label>
-		<label>
-			Description
-			<textarea name="description" cols="50"><?php print($scene->description); ?></textarea>
-		</label>
+		<input type="hidden" name="id" value="<?php print($scene->id); ?>" >
+		<div class="form_block" >
+			<p>			
+				<label>Title</label>
+				<input type="text" name="title" size="50" value="<?php print($scene->title); ?>" />
+			</p>
+			<p>
+				<label>Description</label>
+				<textarea name="description" cols="50"><?php print($scene->description); ?></textarea>
+			</p>
+			<p>
+				<label>Scene Value:</label>			
+				<input type="text" name="value" value="<?php print($scene->value); ?>" >
+			</p>
+		</div>
+		<div class="form_block" >
 		<?php if ($scene->id > 0) { ?>
-		<label>
-			Image filename:
+			<label>Image filename:</label>			
 			<input type="hidden" name="image_id" value="<?php print($scene->image_id); ?>" >
 			<input type="text" name="image_filename" value="<?php print($scene->filename); ?>" >
-			<a href="<?php print($assign_image_link); ?>&KeepThis=true&TB_iframe=true" class="thickbox" >Assign Image</a>
-		</label>
-		<?php } ?>
-		<label>
-			Scene Value:
-			<input type="text" name="value" value="<?php print($scene->value); ?>" >
-		</label>
-		<input type="submit" name="submit" value="Save" />
+			<a href="<?php print($assign_image_link); ?>&KeepThis=true&TB_iframe=true" class="thickbox" >Assign Image</a>		
+		<?php } ?>			
+		</div>
+		<div class="form_block" >
+			<p>		
+			<input type="submit" name="submit" value="Save" />
+			</p>
+		</div>
 	</form>
 </fieldset>
