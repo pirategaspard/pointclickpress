40b3598a1d776ac711224789aeeda1694f804d8c
pirategaspard <dan@danielgaspar.com>
2010-11-05 23:00:21 +0000

    UI updates

--- PCP/views/admin/user/form_login.php
+++ PCP/views/admin/user/form_login.php
@@ -1,10 +1,15 @@
 <?php 
 	//var_dump($_SESSION);
 ?>
-
-<h3>Please Login</h3>
-<form method="post" action="<?php echo($login_form_action); ?>" >
-	Username: <input type="text" id="username" name="username" value="" >
-	Password: <input type="password" id="password" name="password" value="" >
-	<input type="submit" name="submit" value="Log in" />
-</form>
+<fieldset class="ui-helper-reset ui-widget-content ui-corner-all login" >	
+	<h3>Please Login</h3>
+	<form method="post" action="<?php echo($login_form_action); ?>" >
+		<div class="form_block" >
+			Username: <input type="text" id="username" name="username" value="" >
+		</div>
+		<div class="form_block" >
+			Password: <input type="password" id="password" name="password" value="" >
+		</div>
+		<input type="submit" name="submit" value="Log in" class="ui-widget ui-state-default ui-corner-all button login" />
+	</form>
+</fieldset>
