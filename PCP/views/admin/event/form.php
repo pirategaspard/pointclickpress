fb31a38a9971791147c123753762d83ac120e7bf
pirategaspard <dan@danielgaspar.com>
2011-01-10 01:41:03 +0000

    itemstate events

--- PCP/views/admin/event/form.php
+++ PCP/views/admin/event/form.php
@@ -6,6 +6,7 @@
 ?>
 	<fieldset class="ui-helper-reset ui-widget-content ui-corner-all">
 		<form method="post" action="<?php echo($event_form_action); ?>">
+		<?php if (isset($itemstate_id)){ ?><input type="hidden" name="itemstate_id" value="<?php echo($itemstate_id); ?>" /> <?php } ?>
 			<?php if (isset($story_id)){ ?><input type="hidden" name="story_id" value="<?php echo($story_id); ?>" /> <?php } ?>
 			<?php if (isset($location_id)){ ?><input type="hidden" name="location_id" value="<?php echo($location_id); ?>" /> <?php } ?>
 			<?php if (isset($scene_id)){ ?><input type="hidden" name="scene_id" value="<?php echo($scene_id); ?>" /> <?php } ?>
--- PCP/views/admin/itemstate/template.php
+++ PCP/views/admin/itemstate/template.php
@@ -1,3 +1,14 @@
 <?php 
 	echo($itemstate_form);	
+	if ($itemstate->id > 0)
+	{
 ?>
+	<div id="accordion" >
+		<h3 ><a href="#">Events</a></h3>
+		<div style='display: none;' >
+			<?php
+				echo($event_list);
+			?>
+		</div>
+	</div><!-- #accordion -->
+<?php } ?>
