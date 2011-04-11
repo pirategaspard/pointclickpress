1de277a4b8e6f663afa7b7c8727b0319c09774d8
pirategaspard <pirategaspard@b5eaa240-b974-4564-bca8-cd2d19826b34>
2011-04-11 22:45:39 +0000

    commented out loading of small image... It looks good, but slows down scene loads

--- trunk/PCP/js/action/REFRESH.js
+++ trunk/PCP/js/action/REFRESH.js
@@ -19,21 +19,20 @@
 	  	else
 	  	{
 			// load low res image background and stretch it to fill scene 
-		  	var css = {
+		  /*	var css = {
 		      	'background-image': 'url('+data.preload_filename+')',
 		      	'background-size':'100% 100%, auto auto',
 		    	'-o-background-size':'100% 100%, auto auto',
 		    	'-moz-background-size':'100% 100%, auto auto',
 		    	'-webkit-background-size':'100% 100%, auto auto'		    	
 		    }
-		  	$('#grid').css(css);
+		  	$('#grid').css(css); */
 	  	}
 	  	 		  	
 	  	// show items
 	  	var cells = $('#grid').children('a');
 	  	for(var n in data.items)
 		{
-			//$(cells[n]).html('<img src="'+data.items[n]+'" />');
 			$(cells[n]).replaceWith('<div n='+n+'><form n='+n+' i='+data.items[n].id+' action="itemclick?n='+n+'" method="post" ><input type="image" src="'+data.items[n].path+'" name="i" value="'+data.items[n].id+'" /></form></div>');
 	  	}
 		// pre-load high res image and then swap background
