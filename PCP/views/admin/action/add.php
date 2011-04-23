2587024f54fa20b62837133486b894919188c221
pirategaspard <dan@danielgaspar.com>
2011-04-23 13:57:03 +0000

    renamed Event to Action in UI

--- PCP/views/admin/action/add.php
+++ PCP/views/admin/action/add.php
@@ -1,2 +1,2 @@
-<a href="<?php echo(Url::site(Route::get('admin')->uri(array('controller'=>'action','action'=>'edit'))).'?'.$add_id.'&action_id=0'); ?>" class="thickbox ui-widget ui-state-default ui-corner-all button" >Add Event</a>
+<a href="<?php echo(Url::site(Route::get('admin')->uri(array('controller'=>'action','action'=>'edit'))).'?'.$add_id.'&action_id=0'); ?>" class="thickbox ui-widget ui-state-default ui-corner-all button" >Add Action</a>
 
