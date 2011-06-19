f877f6c59d5a54e6294d39aa64fdb0c701eea8b8
pirategaspard <dan@danielgaspar.com>
2011-06-19 01:56:02 +0000

    js for message updated

--- PCP/js/action/MESSAGE.js
+++ PCP/js/action/MESSAGE.js
@@ -5,17 +5,17 @@
 	$.fn.MESSAGE = function(data) 
 	{  		
 		var t = new Date().getTime();
-		var div = $('#message');
+		var div = $('#message>.container');
 		// if message div doesn't exist, create it			
 		if (div.length == 0)
 		{		
 			// if message div doesn't exist, create it
-			$('#description').before('<p id="message"></p>');
-			div = $('#message');
+			$('#description').before('<p id="message"><span class="container"></span></p>');
+			div = $('#message>.container');
 		}
 
 		// add message to message div
-		div.html('<span id="'+t+'">'+data.message+'</span>');
+		div.html('<span class="text" id="'+t+'">'+data.message+'</span>');
 		//div.append('<span id="'+t+'">'+data.message+'</span>');
 		
 		// get wait time
@@ -24,11 +24,11 @@
 			// if we specified a wait time use that
 			wait_time = data.wait_time; 
 			// clear message after waiting			
-			setTimeout(function(){div.remove();},wait_time);
+			setTimeout(function(){$('#message').remove();},wait_time);
 		}
 				
 		//clear on click
-		div.click(function(){div.remove();});
+		div.click(function(){$('#message').remove();});
 				
 	};
 })( jQuery );
