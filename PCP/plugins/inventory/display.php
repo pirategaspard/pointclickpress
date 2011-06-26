b0e48e5f80ed2afedcd58b6ca147cedcb35ca787
pirategaspard <dan@danielgaspar.com>
2011-06-26 13:46:26 +0000

    updated inventory plugin

--- PCP/plugins/inventory/display.php
+++ PCP/plugins/inventory/display.php
@@ -4,7 +4,18 @@
 		<?php	 
 			include(Kohana::find_file('plugins'.DIRECTORY_SEPARATOR.'inventory','css'));
 			include(Kohana::find_file('plugins'.DIRECTORY_SEPARATOR.'inventory','inventory','css'));
-			include(Kohana::find_file('plugins'.DIRECTORY_SEPARATOR.'inventory','inventory.js'));	
+		//	include(Kohana::find_file('plugins'.DIRECTORY_SEPARATOR.'inventory','inventory.js'));
+		
+		echo '<style>';
+		if (plugins_inventory::getCurrentItem() > 0)
+		{
+			echo '#inventory #dropcurrentitem {display: block;}';		
+		}
+		else
+		{
+			echo '#inventory #dropcurrentitem {display: none;}';
+		}
+		echo '</style>';
 		?>
 		
 	</head>
@@ -20,17 +31,16 @@
 					echo '<ul>';
 					foreach ($inventory_items['griditems'] as $item)
 					{						
-						if (plugins_inventory::getCurrentItem() == $item['id']) {echo '<li class="active">';}
-						else {echo '<li >';}				
+						if (plugins_inventory::getCurrentItem() == $item['id']) {echo '<li class="active inventory_item">';}
+						else {echo '<li class="inventory_item">';}				
 						$itemstate = Model_Inventory::getInventoryItemStateByItemId(array('id'=>$item['itemstate_id']));
-						echo '<a id="setcurrentitem" class="inventory_item" href="'.Kohana::$base_url.'announceEvent?event='.INVENTORY_SET_SELECTED_ITEM.'&i='.$item['id'].'"><img src="'.$story->getMediaPath().$itemstate->getPath(THUMBNAIL_IMAGE_SIZE).'" alt="'.$itemstate->description.'" title="'.$itemstate->description.'" /></a>';
+						echo '<a id="setcurrentitem" href="'.Kohana::$base_url.'announceEvent?event='.INVENTORY_SET_SELECTED_ITEM.'&i='.$item['id'].'"><img src="'.$story->getMediaPath().$itemstate->getPath(THUMBNAIL_IMAGE_SIZE).'" alt="'.$itemstate->description.'" title="'.$itemstate->description.'" /></a>';
 						echo '</li>';
 					}
 					echo '</ul>';
-					if (plugins_inventory::getCurrentItem() > 0)
-					{
-						echo '<a id="dropcurrentitem" href="'.Kohana::$base_url.'announceEvent?event='.INVENTORY_DROP_SELECTED_ITEM.'" class="ui-widget ui-state-default ui-corner-all button delete">Drop Selected Item</a>';
-					}
+					echo '<div style="clear:both;" >';
+					echo '<br /><a id="dropcurrentitem" href="'.Kohana::$base_url.'announceEvent?event='.INVENTORY_DROP_SELECTED_ITEM.'" class="ui-widget ui-state-default ui-corner-all button delete">Drop Selected Item</a><br /><br />';
+					echo '</div >';
 				}
 				else
 				{
@@ -45,28 +55,47 @@
 		<script >
 		
 			// get objs
-			var backlink = $('#back_link');
-			var setcurritem = $('#setcurrentitem');
-			var dropcurritem = $('#dropcurrentitem');
+			var backlink = $('#back_link');			
+			//var dropcurritem = $('#dropcurrentitem');
 										
-			// get urls
-			var setlink = setcurritem.attr('href');
-			var droplink = dropcurritem.attr('href');
+			// get urls			
+			//var droplink = dropcurritem.attr('href');
 			
 			// remove links - if they have javascript they don't need these
-			if (backlink) { backlink.attr('href','#'); } 
-			setcurritem.attr('href','#');
-			dropcurritem.attr('href','#');
+			if (backlink) { backlink.attr('href','#'); } 			
+			//dropcurritem.removeAttr('href');
 			
 			// change functionality to be all 'ajaxy'
-			if (backlink) {  backlink.click(function(){ tb_remove(); }) }
-			setcurritem.click(function(){$.post(setlink)});  
-			dropcurritem.click(function(){$.getJSON(droplink, function(events){ $().REFRESH_ITEMS(events[0].data);   }); });
+			if (backlink) {  backlink.click(function(){ tb_remove(); }) }			  
+			//dropcurritem.click(function(){$.getJSON(droplink, function(events){ $().REFRESH_ITEMS(events[0].data);   }); });
+			
+			// remove links on inv items
+			var inv_items = $('.inventory_item>a');	
+			inv_items.each(function(){
+					var item = $(this);						
+					var setlink = item.attr('href');
+					item.attr('h',setlink);
+					item.removeAttr('href');					
+				});
+			
 			
 			$('.inventory_item').click(function()
 			{
-				//$('.active').removeClass('active');
-				$(this).parent().toggleClass('active');
+				var item = $(this);
+				if (item.hasClass('active'))
+				{					
+					item.removeClass('active');
+					$('#dropcurrentitem').hide();	
+					$.post("<?php echo Kohana::$base_url.'announceEvent?event='.INVENTORY_SET_SELECTED_ITEM.'&i=0'; ?>")			
+				}
+				else
+				{
+					var link = item.children('a').attr('h');
+					$('.active').removeClass('active');
+					item.addClass('active');
+					$.post(link);
+					$('#dropcurrentitem').show();
+				}
 			});
 			
 						
