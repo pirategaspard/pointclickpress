8ef32fb46ebbbbde978024efb8c82111ca06c0bf
pirategaspard <pirategaspard@b5eaa240-b974-4564-bca8-cd2d19826b34>
2011-10-10 17:35:28 +0000

    added inventorydrop action

---
+++ trunk/PCP/plugins/inventory/classes/action/inventorydrop.php
@@ -0,0 +1,42 @@
+<?php 
+/*
+	Removes currently selected item from inventory and adds it to a scene
+ */
+define('INVENTORY_DROP_ITEM','INVENTORY_DROP_ITEM');
+class action_inventorydrop extends Model_Base_PCPActionDef
+{	
+	protected $label = 'Inventory Drop'; 
+	protected $description = 'Drop item from inventory. "scene_id,cell_id(,griditem_id)"';
+	protected $allowed_action_types = array(INVENTORY_DROP_ITEM);
+	protected $events = array(INVENTORY_DROP_ITEM);
+	
+	public function performAction($args=array(),$hook_name='')
+	{
+		$action_data = $this->tokenize($args['action_value']);
+		if (count($action_data) >= 2)
+		{
+			// you can specify a griditem_id, otherwise default is currently selected item
+			if (count($action_data) ==3)
+			{
+				$griditem_id = $action_data[3];
+			}
+			else
+			{
+				$griditem_id = self::getCurrentItem();
+			}
+			$scene_id = $action_data[0];
+			$cell_id = $action_data[1];
+			// if we have a selected griditem
+			if(griditem_id > 0)
+			{
+				// put current item into a scene in a cell
+				Items::setGridItemLocation($griditem_id,$scene_id,$cell_id); 
+				Storydata::set('current_item',0);
+			}
+		}
+		$refresh = new action_refreshitems;
+		$results = $refresh->performAction($args,$story_data);
+		return $results;
+	}
+}
+?>
